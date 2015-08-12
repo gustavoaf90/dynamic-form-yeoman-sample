413d191deb8f27e52aafa61f33bc9dba562f2149
gustavo.falleiros <gustavo.falleiros@neppo.com.br>
2015-08-12 15:00:07 -0300

    custom directive

--- app/scripts/directives/dynamicform.js
+++ app/scripts/directives/dynamicform.js
@@ -9,7 +9,9 @@
 angular.module('testApp')
   .directive('dynamicForm', function () {
     return {
-      template: '<div></div>',
+      template: '<dynamic-form>
+      				
+      			</dynamic-form>',
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
         element.text('this is the dynamicForm directive');
